34828adb1e527dabf6beb9eefd3f3e1a7389ae75
Harshit-joshi1997 <joshiharshit291@gmail.com>
2025-08-27 18:47:50 +0530

    phone no validation updated

--- src/components/Forms.tsx
+++ src/components/Forms.tsx
@@ -67,7 +67,7 @@ const Forms = () => {
               type="tel"
               placeholder="Enter your phone number"
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-              {...register('phone',{ maxLength: 15 })}
+              {...register('phone',{ maxLength: 15 ,minLength:10})}
             />
           </div>
 
